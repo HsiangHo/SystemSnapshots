f01654939b21913faa70f17b70356c7591f20b94
HsiangHo <ho_cheung@qq.com>
2018-12-28 21:49:16 +0800

    initialize project

---
+++ System Snapshots/System Snapshots/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  System Snapshots
+//
+//  Created by Jovi on 12/28/18.
+//  Copyright © 2018 Jovi. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ System Snapshots/System Snapshots/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  System Snapshots
+//
+//  Created by Jovi on 12/28/18.
+//  Copyright © 2018 Jovi. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
